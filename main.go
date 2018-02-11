3347e71ac2e4ee7c7333136be0cb4ae8007d742c
Yuki Yugui Sonoda <yugui@yugui.jp>
2018-02-11 15:19:44 +0900

    refactor the core function into smaller functions

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -10,20 +11,25 @@ import (
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
 )
 
-func run() error {
-	var req plugin.CodeGeneratorRequest
-	buf, err := ioutil.ReadAll(os.Stdin)
+func parseReq(r io.Reader) (*plugin.CodeGeneratorRequest, error) {
+	buf, err := ioutil.ReadAll(r)
 	if err != nil {
-		return err
-	}
-	if err = proto.Unmarshal(buf, &req); err != nil {
-		return err
+		return nil, err
 	}
 
+	var req plugin.CodeGeneratorRequest
+	if err = proto.Unmarshal(buf, &req); err != nil {
+		return nil, err
+	}
+	return &req, nil
+}
+
+func processReq(req *plugin.CodeGeneratorRequest) *plugin.CodeGeneratorResponse {
 	files := make(map[string]*descriptor.FileDescriptorProto)
 	for _, f := range req.ProtoFile {
 		files[f.GetName()] = f
 	}
+
 	var resp plugin.CodeGeneratorResponse
 	for _, fname := range req.FileToGenerate {
 		f := files[fname]
@@ -33,13 +39,27 @@ func run() error {
 			Content: proto.String(proto.MarshalTextString(f)),
 		})
 	}
+	return &resp
+}
 
-	buf, err = proto.Marshal(&resp)
+func emitResp(resp *plugin.CodeGeneratorResponse) error {
+	buf, err := proto.Marshal(resp)
 	if err != nil {
 		return err
 	}
 	_, err = os.Stdout.Write(buf)
 	return err
+}
+
+func run() error {
+	req, err := parseReq(os.Stdin)
+	if err != nil {
+		return err
+	}
+
+	resp := processReq(req)
+
+	return emitResp(resp)
 }
 
 func main() {
